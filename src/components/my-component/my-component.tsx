9fb559166db2fafafb5bc88132f0993a7f067797
chandrashekhar-moger <chandrashekhar-moger@hpe.com>
2021-08-18 10:14:11 +0530

    Initial commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,29 @@
+import { Component, Prop, h, State } from '@stencil/core';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  detailBox: HTMLElement;
+  showDetails = () => {
+    this.detailBox.classList.remove('display-none');
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.showDetails}>Show detail</button>
+        <span
+          id="detail"
+          ref={elem => {
+            this.detailBox = elem;
+          }}
+          class="display-none"
+        >
+          <slot></slot>
+        </span>
+      </div>
+    );
+  }
+}
